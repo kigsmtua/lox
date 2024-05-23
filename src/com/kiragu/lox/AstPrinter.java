42341fd2e40806c9883360fd4cda4a3887a7d575
John Kiragu <mutuakiragu@gmail.com>
2024-05-23 13:57:28 +0300

    modify the AST printer method

---
+++ src/com/kiragu/lox/AstPrinter.java
@@ -0,0 +1,23 @@
+package com.kiragu.lox;
+
+public class AstPrinter implements Expr.Visitor<String>{
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return null;
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return null;
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        return null;
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return null;
+    }
+}
