4d9d9bda0202bb783034f10718d82ed38e1363bc
John Kiragu <mutuakiragu@gmail.com>
2024-07-09 16:15:31 +0300

    Lets visit the expression statement

--- src/com/kiragu/lox/Lox.java
+++ src/com/kiragu/lox/Lox.java
@@ -15,17 +15,14 @@ public class Lox {
 
     private static final Interpreter interpreter = new Interpreter();
     public static void main(String[] args) throws IOException {
-//        Expr expression = new Expr.Binary(
-//                new Expr.Unary(
-//                        new Token(TokenType.MINUS, "-", null, 1),
-//                        new Expr.Literal(123)),
-//                new Token(TokenType.STAR, "*", null, 1),
-//                new Expr.Grouping(
-//                        new Expr.Literal(45.67)));
-//
-//        System.out.println(new AstPrinter().print(expression));
-        System.out.println(args.toString());
-        run("2+3");
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
     }
     public static void runPrompt() throws IOException {
         InputStreamReader input = new InputStreamReader(System.in);
--- src/com/kiragu/tools/GenerateAst.java
+++ src/com/kiragu/tools/GenerateAst.java
@@ -22,7 +22,8 @@ public class GenerateAst {
 
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "Expression : Expr expression",
-                "Print      : Expr expression"
+                "Print      : Expr expression",
+                "Var        : Token name, Expr initializer"
         ));
     }
     private static void defineAst(
