aec2194e0b3edb91e16f9190a5d63d19ac84963e
John Kiragu <mutuakiragu@gmail.com>
2024-07-09 18:24:52 +0300

    Lets add support for blocks

--- src/com/kiragu/lox/Environment.java
+++ src/com/kiragu/lox/Environment.java
@@ -5,7 +5,15 @@ import  java.util.Map;
 public class Environment {
     private final Map<String, Object> values = new HashMap<>();
 
+    final Environment enclosing;
 
+    Environment() {
+        enclosing = null;
+    }
+
+    Environment(Environment enclosing) {
+        this.enclosing = enclosing;
+    }
     void define(String name, Object value) {
         values.put(name, value);
     }
@@ -15,7 +23,10 @@ public class Environment {
             values.put(name.lexeme, value);
             return;
         }
-        //
+        if (enclosing != null) {
+            enclosing.assign(name, value);
+            return;
+        }
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
@@ -24,7 +35,7 @@ public class Environment {
         if (values.containsKey(name.lexeme)) {
             return values.get(name.lexeme);
         }
-
+        if (enclosing != null) return enclosing.get(name);
         throw new RuntimeError(name,
                 "Undefined variable '" + name.lexeme + "'.");
     }
